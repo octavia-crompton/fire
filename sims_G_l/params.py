ad516a7ab8792c208a1254ebfb0bd3c073981cf8
Octavia Crompton <quatratavia@gmail.com>
2020-04-08 12:00:58 +0800

    fixing RCSR_analytic-random

--- sims_G_l/params.py
+++ sims_G_l/params.py
@@ -3,7 +3,8 @@ import numpy as np
 all_params = {
       "batch_dict" :    {               
         "beta" : [0.5],
-        "alpha" :  [0.02, 0.04],
+        "S" : [0.5, 0.2],
+        "alpha" :  [0.02, 0.04, 0.06],
         "ignition_type" : ["G_l", "random"]
         },
       "sim_dict" : {        
@@ -14,14 +15,13 @@ all_params = {
             "k_u" : 20.0,
             "k_l" : 5.0,    
             "r_u" : 0.25,
-            "r_l" : 1.5,                    
-            "S" : 0.5,
+            "r_l" : 1.5,  
             "dt" : 0.01,
-            "dt_p" : 0.1,            
+            "dt_p" : 0.1, 
             "seed" : 0,
-            "ti" : 3000,        
-            "tmax" : 1000,                  
-            "chi" : 1,                           
+            "ti" : 3000, 
+            "tmax" : 10000,
+            "chi" : 1,
             "severity_type" : "random",
             "std_severity" : 0.1,
             "r" : 0.5, 
